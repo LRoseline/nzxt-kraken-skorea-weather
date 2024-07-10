ab8754ce2930b468ad038c03bf8f73841a75a587
LRoseline <amb17@naver.com>
2024-07-11 04:09:05 +0900

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cpus, setCpus] = useState(null);
+    const [gpus, setGpus] = useState(null);
+    const [ram, setRam] = useState(null);
+    const [kraken, setKraken] = useState(null);
+
+    const [liquid, setLiquid] = useState(null);
+    const [weather, setWeather] = useState("01d");
+    const [outTemp, setOutTemp] = useState("23");
+
+    const weatherAxi = () => {
+        axios.get("https://api.tsukimorifriends.xyz/api/weather/current/온천동?authkey="+process.env.REACT_APP_TEST_KEY).then(r => {
+            setWeather(r.data.body.weather.current.weather[0].icon);
+            setOutTemp(Math.floor(r.data.body.weather.current.temp));
+        }).catch(e => {
+            setWeather("Error Data");
+        });
+    }
+
+    useEffect(() => {
+        window.nzxt = {
+            v1: {
+                onMonitoringDataUpdate: (data) => {
+                    const { cpus, gpus, ram, kraken } = data;
+
+                    setCpus(Math.floor(cpus[0].temperature));
+                    setGpus(gpus);
+                    setRam(ram);
+                    setKraken(kraken);
+
+                    setLiquid(kraken.liquidTemperature);
+                }
+            }
+        };
+    }, []);
+
+
+    const [clock, setClock] = useState("00:00");
+
+    const timeFormatZ = (ints) => {
+        if (ints < 10) {
+            return ints = "0"+ints;
+        }
+
+        return ints;
+    }
+
+    useEffect(() => {
+        const Timer = setInterval(() => {
+            let time = new Date();
+
+            setClock(timeFormatZ(time.getHours())+":"+timeFormatZ(time.getMinutes()));
+        }, 1000);
+
+        weatherAxi();
+
+        const weatherTimer = setInterval(() => {
+            weatherAxi();
+        }, 1000*60*60);
+
+        return () => {
+            clearInterval(Timer);
+            clearInterval(weatherTimer);
+        }
+    });
+
+    return (
+        <div>
+            <div className="elias" style={{backgroundImage: `radial-gradient(#0000, #000F), url(${"/weather/"+weather+".png"})`}}>
+                <div className="circle">
+                    <div className="clock">{clock}</div>
+                    <div className="cpu">
+                        <div>
+                            <div className="tabo">CPU</div>
+                            <div className="tabo-value">{cpus}</div>
+                        </div>
+                        <div>
+                            <div className="tabo">WATER</div>
+                            <div className="tabo-value">{liquid}</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div>OUTSIDE</div>
+                    <div style={{fontSize: "1cm"}}>{outTemp}</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
