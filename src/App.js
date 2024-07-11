5b00c0ee40f7ec08f333306a7b58e9a31eada3c7
LRoseline <amb17@naver.com>
2024-07-11 14:16:51 +0900

    dust

--- src/App.js
+++ src/App.js
@@ -8,21 +8,29 @@ function App() {
     const [ram, setRam] = useState(null);
     const [kraken, setKraken] = useState(null);
 
-    const [liquid, setLiquid] = useState(null);
+    const [cpuT,setCpuT] = useState(40);
+    const [liquid, setLiquid] = useState(30);
     const [weather, setWeather] = useState("01d");
-    const [outTemp, setOutTemp] = useState("23");
+    const [outTemp, setOutTemp] = useState("-20");
+
+    const [pm10v, setPm10v] = useState("000");
+    const [pm25v, setPm25v] = useState("000");
+
+    const [pm10g, setPm10g] = useState("000");
+    const [pm25g, setPm25g] = useState("000");
 
     useEffect(() => {
         window.nzxt = {
             v1: {
                 onMonitoringDataUpdate: (data) => {
-                    const { cpus, gpus, ram, kraken } = data;
+                    const {cpus, gpus, ram, kraken} = data;
 
-                    setCpus(Math.floor(cpus[0].temperature));
+                    setCpus(cpus);
                     setGpus(gpus);
                     setRam(ram);
                     setKraken(kraken);
 
+                    setCpuT(Math.floor(cpus[0].temperature));
                     setLiquid(kraken.liquidTemperature);
                 }
             }
@@ -41,13 +49,38 @@ function App() {
     }
 
     const weatherAxi = () => {
+        const dustGrade = (value) => {
+            switch (value) {
+                case -1:
+                    return "#aa80ff"
+                case 1:
+                    return "#66e0ff"
+                case 2:
+                    return "#66ff66"
+                case 3:
+                    return "#ff9933"
+                case 4:
+                    return "#ff3333"
+                default:
+                    return "#ffffff";
+            }
+        };
+
         axios.get("https://api.tsukimorifriends.xyz/api/weather/current/온천동?authkey="+process.env.REACT_APP_PRODUCT_KEY).then(r => {
-            setWeather(r.data.body.weather.current.weather[0].icon);
-            setOutTemp(Math.floor(r.data.body.weather.current.temp));
+            const result = r.data.body;
+            setWeather(result.weather.current.weather[0].icon);
+            setOutTemp(Math.floor(result.weather.current.temp));
+
+            setPm10v(result.dust.pm10v);
+            setPm25v(result.dust.pm25v);
+
+            setPm10g(dustGrade(result.dust.pm10g));
+            setPm25g(dustGrade(result.dust.pm25g));
         }).catch(e => {
             setWeather("Error Data");
         });
     }
+
 
     useEffect(() => {
         const weatherTimer = setInterval(() => {
@@ -83,17 +116,31 @@ function App() {
                     <div className="cpu">
                         <div>
                             <div className="tabo">CPU</div>
-                            <div className="tabo-value">{cpus}</div>
+                            <div className="tabo-value">{cpuT}</div>
                         </div>
                         <div>
-                            <div className="tabo">WATER</div>
+                            <div className="tabo">AIO</div>
                             <div className="tabo-value">{liquid}</div>
                         </div>
                     </div>
                 </div>
                 <div className="bottom">
-                    <div>OUTSIDE</div>
-                    <div style={{fontSize: "1cm"}}>{outTemp}</div>
+                    <div className="pm10">
+                        <div>
+                            <span style={{boxShadow: '0 3px 0 '+pm10g}}>PM10</span>
+                        </div>
+                        <div className="bottom-value-small">{pm10v}</div>
+                    </div>
+                    <div className="outside">
+                        <div>OUTSIDE</div>
+                        <div className="bottom-value">{outTemp}</div>
+                    </div>
+                    <div className="pm25">
+                        <div>
+                            <span style={{boxShadow: '0 3px 0 '+pm25g}}>PM25</span>
+                        </div>
+                        <div className="bottom-value-small">{pm25v}</div>
+                    </div>
                 </div>
             </div>
         </div>
