3ce7697a4bafa8825020fef1bc335ba972c3add7
LRoseline <amb17@naver.com>
2024-07-19 21:38:57 +0900

    oks

--- src/JsonEx.js
+++ src/JsonEx.js
@@ -1,31 +1,48 @@
 import { useState, useEffect } from 'react';
+import './setting.css';
 
 function JsonEx() {
+    const [defaultMemo, setDefaultMemo] = useState(null);
+
+    const exportJson = () => {
+        console.log("JUDGE");
+        const resultJson = new Object();
+
+        const weathers = new Object();
+        weathers.key = document.querySelector("#api-weather").value;
+        weathers.lat = document.querySelector("#lat").value;
+        weathers.lon = document.querySelector("#lon").value;
+        resultJson.weather = weathers;
+
+        const gokr = new Object();
+        gokr.key = document.querySelector("#api-datagokr").value;
+        gokr.station = document.querySelector("#station").value;
+        resultJson.datagokr = gokr;
+
+        setDefaultMemo(JSON.stringify(resultJson));
+    }
 
     return (
-        <div>
+        <main>
             <div>
-                <div>
-                    <label for="openweathermap">Openweathermap</label>
-                    <input id="openweathermap" type="text" />
-                    <div>
-                        <label for="lat">lat</label>
-                        <input id="lat" type="number" />
-                    </div>
-                    <div>
-                        <label for="lon">lon</label>
-                        <input id="lon" type="number" />
+                <div className="object">
+                    <h2>Openweathermap</h2>
+                    <hr />
+                    <div className="table">
+                        <input id="api-weather" required placeholder="API키" type="text" />
+                        <input id="lat" required placeholder="lat" type="number" />
+                        <input id="lon" required placeholder="lon" type="number" />
                     </div>
                 </div>
-                <div>
-                    <label for="datagokr">datagokr</label>
-                    <input id="datagokr" type="text" />
-                    <div>
-                        <label for="station">측정소</label>
-                        <input id="station" type="text" />
+                <div className="object">
+                    <h2>공공데이터포털 - 미세먼지 정보</h2>
+                    <hr />
+                    <div className="table">
+                        <input id="api-datagokr" required placeholder="API키" type="text" />
+                        <input id="station" required placeholder="측정소 이름" type="text" />
                     </div>
                 </div>
-                <button>출력</button>
+                <button onClick={exportJson}>출력</button>
             </div>
             <div>
                 <h3>사용방법</h3>
@@ -38,14 +55,12 @@ function JsonEx() {
                 </ol>
                 <ul>
                     <li>여러분이 가진 인증키는 일절 수집하지 않습니다.</li>
-                    <li>날씨 좌표는 꼭 입력해주세요.</li>
+                    <li>날씨 좌표는 꼭 입력해주세요. 좌표값은 Openweathermap에서 현재 위치를 찾은 다음 해당 값으로 입력해주세요.</li>
                     <li>미세먼지 측정소는 스마트폰 날씨 앱 기준으로 나오는 현재 측정소로 입력해주세요.</li>
                 </ul>
             </div>
-            <textarea id="result-json" readOnly>
-                ㅁ?ㄹ
-            </textarea>
-        </div>
+            <textarea id="result-json" readOnly defaultValue={defaultMemo}></textarea>
+        </main>
     );
 }
 
